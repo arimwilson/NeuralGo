e825f241b7d4d91b5500176be04936c7fb3064da
Ari Wilson <ari.wilson@gmail.com>
2016-04-17 21:22:47 -0700

    softmax

--- neural/activation_function.go
+++ neural/activation_function.go
@@ -25,6 +25,19 @@ func NewActivationFunction(name ActivationName) ActivationFunction {
     return func(x mat64.Matrix, y *mat64.Dense) {
       y.Apply(func(r, c int, v float64) float64 { return math.Tanh(v) }, x)
     }
+  case ActivationName_SOFTMAX:
+    return func(x mat64.Matrix, y *mat64.Dense) {
+      r, c := x.Dims()
+      for i := 0; i < r; i++ {
+        exp_sum := 0.0
+        for j := 0; j < c; j++ {
+          exp_sum = exp_sum + math.Exp(x.At(i, j))
+        }
+        for j := 0; j < c; j++ {
+          y.Set(i, j, math.Exp(x.At(i, j)) / exp_sum)
+        }
+      }
+    }
   }
   return nil
 }
@@ -59,6 +72,20 @@ func NewDActivationFunction(name ActivationName) DActivationFunction {
         tanh := math.Tanh(v)
         return 1 - tanh * tanh
       }, y)
+    }
+  case ActivationName_SOFTMAX:
+    return func(y mat64.Matrix, x *mat64.Dense) {
+      // TODO(ariw): Finish this.
+      r, c := y.Dims()
+      for i := 0; i < r; i++ {
+        exp_sum := 0.0
+        for j := 0; j < c; j++ {
+          exp_sum = exp_sum + math.Exp(y.At(i, j))
+        }
+        for j := 0; j < c; j++ {
+          x.Set(i, j, math.Exp(y.At(i, j)) / exp_sum)
+        }
+      }
     }
   }
   return nil
